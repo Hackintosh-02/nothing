8fdb93fcacb0626932e9c6c13e9449975b72ec0c
Hackintosh-02 <97759154+Hackintosh-02@users.noreply.github.com>
2023-04-10 22:58:00 +0530

    uploaded
    asdf

---
+++ first.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+#ifndef ONLINE_JUDGE
+	freopen("input1.txt","r",stdin);
+	freopen("output1.txt","r",stdout);
+
+	cout<<"HI";
+	string s;
+	cin>>s;
+	cout<<s;
+}
